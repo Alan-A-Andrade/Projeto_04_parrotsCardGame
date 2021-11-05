a82c28531dbbbcd96ae08cffc6dd1ef7ecf6bcc3
Alan <alanaa92@gmail.com>
2021-11-05 09:48:31 -0300

    Implementação do check de vitoria e virada de carta

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-let contador = 0;
-let contador2 = 0;
+let contador = 0; //para array de cartas
+let contador2 = 0; //para lista de cartas
 let qntCartas = "";
+let qntCartasCorretas = 0
+let qntCartasViradas = 0
 let listaCartas = document.querySelector("ul");
 let arrayCartas = [];
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+let carta = null;
+let cartaAnterior = null;
 
 qntCartas = prompt("quantas cartas?");
 
 //continuar perguntando numero de cartas até obter resposta valida
 
 
-while(qntCartas % 2 !== 0 || qntCartas < 4 || qntCartas > 14){
+while (qntCartas % 2 !== 0 || qntCartas < 4 || qntCartas > 14) {
   qntCartas = prompt("quantas cartas?");
 
-  }
+}
 
 
 
 
 //criar array para identificação de cartas de n(minimo2) até m(maximo14)
-while(contador< ( qntCartas / 2) ) {
+while (contador < (qntCartas / 2)) {
 
- 
-arrayCartas.push(contador+1);
-arrayCartas.push(contador+1);
 
-contador++
- 
+  arrayCartas.push(contador + 1);
+  arrayCartas.push(contador + 1);
+
+  contador++
+
 }
 
 //Mistura Array
 misturar();
 
 
+//Adiciona cartas ao tabuleiro
 
-
-//adicionar cartas como lista
-while(qntCartas % 2 !== 0 || qntCartas < 4 || qntCartas > 14){
-qntCartas = prompt("quantas cartas?");
-}
-
-while(contador2<qntCartas){
-let carta;
-carta = `<div class="carta" data-identifier="card"> 
+while (contador2 < qntCartas) {
+  let carta;
+  carta = `<div onclick="selecionarCarta(this)" class="carta" data-identifier="card"> 
     
 <div class="verso" data-identifier="back-face">
 </div>
@@ -51,23 +50,94 @@ carta = `<div class="carta" data-identifier="card">
 <img src="./assets/Pictures/parrot_0${arrayCartas[contador2]}.gif">
 </div>`
 
-adicionarCarta(carta);
-contador2++
+  adicionarCarta(carta);
+  contador2++
+
+}
+
+//carta selecionada pelo usuario
+
+function selecionarCarta(cartaclicada) {
+
+  carta = cartaclicada;
+
+  if (cartaAnterior === null) {
+
+    virarcarta(cartaclicada);
+    cartaclicada.classList.add("selecionada");
+    qntCartasViradas++;
+    cartaAnterior = cartaclicada;
+  }
+
+  else if (cartaclicada === cartaAnterior) {
+
+  }
+
+  else {
+    virarcarta(cartaclicada);
+    cartaclicada.classList.add("selecionada");
+    qntCartasViradas++
+    setTimeout(function () {
+      acertouCarta(cartaclicada, cartaAnterior)
+    }, 1000);
+  }
+
 
 }
 
 function misturar() {
-arrayCartas.sort(comparador);
+  arrayCartas.sort(embaralhador);
 }
 
-// Após esta linha, a minhaArray estará embaralhada
 
-
-function comparador() { 
-	return Math.random() - 0.5; 
+function embaralhador() {
+  return Math.random() - 0.5;
 }
 
 
 function adicionarCarta(carta) {
-    listaCartas.innerHTML += `<li>${carta}</li>`;
+  listaCartas.innerHTML += `<li>${carta}</li>`;
+}
+
+function virarcarta(elemento) {
+  let verso = elemento.querySelector(".verso")
+  let frente = elemento.querySelector(".frente")
+  verso.classList.toggle("virarverso")
+  frente.classList.toggle("virarfrente")
+}
+
+function acertouCarta(elemento, elementoAnterior) {
+
+  if (elementoAnterior.innerHTML === elemento.innerHTML) {
+
+    elementoAnterior.removeAttribute("onclick");
+    elemento.removeAttribute("onclick");
+    console.log(elementoAnterior)
+    console.log(elemento)
+    cartaAnterior = null;
+    carta = null;
+    qntCartasCorretas += 2;
+    checarVitoria();
+
+
+  }
+
+  else {
+    virarcarta(elemento);
+    virarcarta(elementoAnterior);
+  }
+
+  elemento.classList.remove("selecionada");
+  elementoAnterior.classList.remove("selecionada");
+  cartaAnterior = null;
+
+}
+
+function checarVitoria() {
+
+  if (qntCartasCorretas == qntCartas) {
+
+    alert(`Você ganhou em ${qntCartasViradas} jogadas`)
+  }
+
 }
